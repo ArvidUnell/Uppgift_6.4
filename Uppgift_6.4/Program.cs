759b12289d0b3c96adf0e8591cece671cb2c64c5
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-14 12:50:17 +0100

    Add project files.

---
+++ Uppgift_6.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Potens(3,2));
+            Console.WriteLine(Potens(6.2,4));
+        }
+        /// <summary>
+        /// Beräknar en potens med heltalsexponent
+        /// </summary>
+        /// <param name="bas">Potensens bas</param>
+        /// <param name="exponent">Potensens exponent</param>
+        /// <returns>Potensens värde</returns>
+        static double Potens(double bas, int exponent)
+        {
+            return Math.Pow(bas, exponent);
+        }
+    }
+}
